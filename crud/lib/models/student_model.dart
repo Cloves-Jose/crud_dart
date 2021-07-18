c75ce4ea2ceae05e43d405f98bc0ca0787df9093
Cloves Jose <clovesjsilva8@gmail.com>
2021-07-18 16:49:20 -0300

    classe modelo Estudante

---
+++ crud/lib/models/student_model.dart
@@ -0,0 +1,20 @@
+//Essa será a classe modelo para a estrutura de dados de estudante
+class Student {
+  int id;
+  String name;
+
+  Student(this.id, this.name);
+
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      'id': id,
+      'name': name,
+    };
+    return map;
+  }
+
+  Student.fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    name = map['name'];
+  }
+}
